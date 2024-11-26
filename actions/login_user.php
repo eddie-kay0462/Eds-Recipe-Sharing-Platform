2c5f15af86f1ddadf83b7a998bf8368fcbdb253a
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2024-11-26 01:33:32 +0000

    Added backend logic to redirect user based of on role

--- actions/login_user.php
+++ actions/login_user.php
@@ -1,10 +1,16 @@
 <?php
-include '../db/config2.php';
+include '../db/config.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['email']) && isset($_POST['password'])) {
+        //get and trim AND sanitize form data
         $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
         $password = $_POST['password'];
+
+        //TRIM AND SANITIZE FORM DATA
+        $email = trim($email);
+        $password = trim($password);
+
 
         $sql = "SELECT * FROM users WHERE email='$email'";
         $result = mysqli_query($conn, $sql);
@@ -14,17 +20,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if (password_verify($password, $user['password'])) {
                 session_start();
                 $_SESSION['user'] = $user;
-                header('Location: ../view/explore_recipes.php');
-                exit;
-            } else {
-                // Redirect back with an error message for invalid password
-                header('Location: ../view/login.php?error=Invalid email or password');
-                exit;
+                $_SESSION['role'] = $user['role'];
+
+                switch ($user['role']) {
+                    case  1:
+                        header('Location: ../view/admin/dashboard.php');
+                        break;
+
+                    case 2:
+                        header('Location: ../view/explore_recipes.php');
+                        break;
+                    default:
+                        header('Location: ../view/explore_recipes.php');
+                        break;
+                }
             }
-        } else {
-            // Redirect back with an error message for invalid email
-            header('Location: ../view/login.php?error=Invalid email or password');
-            exit;
         }
+
+        header('Location: ../view/login.php?error=Invalid email or password');
     }
 }
--- actions/register_user.php
+++ actions/register_user.php
@@ -1,10 +1,10 @@
 <?php
-    include '../db/config.php';
+include '../db/config.php';
 
-   //check if form is submited
-   if($_SERVER['REQUEST_METHOD'] == 'POST'){
-       if (isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirmPassword'])) {
-          //retrieve form data and sanitize
+//check if form is submited
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirmPassword'])) {
+        //retrieve form data and sanitize
         $fname = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_SPECIAL_CHARS);
         $lname = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_SPECIAL_CHARS);
         $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
@@ -12,7 +12,7 @@
         $confirmPassword = $_POST['confirmPassword'];
 
         //check if password and confirm password match
-        if($password != $confirmPassword){
+        if ($password != $confirmPassword) {
             echo 'Password and confirm password do not match';
             exit();
         }
@@ -21,8 +21,9 @@
         $sql = "SELECT * FROM users WHERE email='$email'";
         $result = mysqli_query($conn, $sql);
 
-        if(mysqli_num_rows($result) > 0){
-            echo 'Email already exists';
+        //if email exists, redirect back with an error message, ON THE SAME REGISTER PAGE
+        if (mysqli_num_rows($result) > 0) {
+            header('Location: ../view/register.php?error=Email already exists');
             exit();
         }
 
@@ -33,14 +34,11 @@
         $sql = "INSERT INTO users (fname, lname, email, password) VALUES ('$fname', '$lname', '$email', '$password')";
         $result = mysqli_query($conn, $sql);
 
-        if($result){
+        if ($result) {
             // echo 'User registered successfully';
             header('Location: ../view/explore_recipes.php');
         } else {
             echo 'Failed to register user';
         }
-
-       }
-   }
-
-?>
+    }
+}
